850d7f322ea551f2074789a65976197cf80a72e9
Caio de Camargo <caiodcamargo999@users.noreply.github.com>
2026-09-14 14:54:13 -0300

    feat: redirect root domain to /rarityaudit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/rarityaudit',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
